1e6bf2166f1c32d12c35e2af3459ba9f60281752
Fernanda de Jesus <fernandabussular@gmail.com>
2024-09-24 02:35:39 -0300

    fix tests

--- test/controllers/emails_controller_test.rb
+++ test/controllers/emails_controller_test.rb
@@ -6,8 +6,23 @@ class EmailsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
-    get emails_create_url
+  test "should create email and redirect with notice" do
+    user = users(:one)
+    post send_email_url, params: { user_id: user.id, subject: "Test Subject", body: "Test Body" }
+
+    assert_redirected_to root_path
+
+    follow_redirect!
     assert_response :success
+    assert_select "div.alert", text: "E-mail enviado com sucesso!"
+  end
+
+
+  test "should redirect with error notice when user_id is blank" do
+    post send_email_url, params: { subject: "Test Subject", body: "Test Body" }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_response :success
+    assert_select "div.alert", text: "Por favor, selecione um usuário antes de enviar o e-mail."
   end
 end
